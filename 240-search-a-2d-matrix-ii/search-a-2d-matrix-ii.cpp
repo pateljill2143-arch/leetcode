3ad51ae74f3d72b8577ab7f807b29e9969157e44
JILL SHAILESH PATEL <pateljill2143@gmail.com>
2026-08-05 19:45:02 +0530

    Time: 750 ms (5.14%) | Memory: 18.6 MB (93.37%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        bool found= false;
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                if(matrix[i][j]==target)
+                {
+                    found=true;
+                }
+            }
+        }
+        return found;
+
+        
+    }
+};
