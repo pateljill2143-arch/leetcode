a722cc4ded8de5eac29adf66aca5d7b92a30bbd0
JILL SHAILESH PATEL <pateljill2143@gmail.com>
2026-09-01 22:13:48 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (8.68%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.length() - 1;
+
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+
+        int c = 0;
+        while (i >= 0 && s[i] != ' ') {
+            c++;
+            i--;
+        }
+
+        return c;
+    }
+};
